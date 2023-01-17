c1b9b53c5aa9beef061ef69bc3d30702890637b9
shiyonghong <mzh9354@sina.com>
2023-01-17 20:04:13 +0800

    Initial Commit

---
+++ ImageTinter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ImageTinter
+//
+//  Created by vino on 2023/1/17.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
